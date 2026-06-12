e13a945dc13e96e65b2c1f26218326e8846a16fd
Auduceus <jackisswag77@gmail.com>
2026-06-12 03:34:11 -0500

    Ran the shit again, but I want these commit changes gone.

--- AlchemyGame/obj/Debug/net9.0/AlchemyGame.AssemblyInfo.cs
+++ AlchemyGame/obj/Debug/net9.0/AlchemyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlchemyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910e181c13896e4af0f9bdcdbdee8345f4745526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59da573f89cc1e8232200dffa75bcc07b601d7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlchemyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlchemyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
